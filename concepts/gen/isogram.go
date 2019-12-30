e5da58b2ffd2933edf03a39aa897151837e92bf3
Lukas Jocham <jochamlu@gmail.com>
2019-12-30 22:24:33 +0100

    update isogram to to be titled as noob but i am a noob bcse all people are that don't put in the  hours of hard work. this cant be me just started 3 years ago. lol

--- concepts/gen/isogram.go
+++ concepts/gen/isogram.go
@@ -1,31 +1,26 @@
-//package main determines if a word is an isogram[true] or not[false]
+// package main determines if a word is an isogram[true] or not[false]
+// the term "isogram" describes a string of text without a single repeating character like the word i s o g r a m
+// not so the word n o o b because of the o occuring twice
 package main
 
 import "fmt"
 
-//runes keeps track of the letters currently in test for an isogram
+// runes keeps track of the letters currently in test for an isogram
 var runes []rune
 
 // IsIsogram determines if a given string "input" is an isogram or not returning
 // boolish with the result true for: is isogram , false for: is not isogram
 func IsIsogram(input string) bool {
-	for _, l := range input {
+	for _ , l := range input {
 		if find(runes, l) {
-			return true
+			return false
 		}
 		runes = append(runes, l)
 	}
-	return false
+	return true
 }
 
-func main() {
-	notIsogram := IsIsogram("isogram") // exp. false
-	isIsogram := IsIsogram("iisogram") // exp. true
-	fmt.Printf("word: isogram, exp. false, got: %v \n", notIsogram)
-	fmt.Printf("word: iisogram, exp. true, got: %v \n", isIsogram)
-}
-
-// find finds first duplicate "l" of type rune in a given rune slice
+// find finds first case independent duplicate "l" of type rune in a given rune slice this is also 
 func find(vals []rune, val rune) bool {
 	for _, v := range vals {
 		if v == val {
@@ -34,3 +29,18 @@ func find(vals []rune, val rune) bool {
 	}
 	return false
 }
+
+func main() {
+	isogram := IsIsogram("isogram") // expected: true
+	emty := IsIsogram("") // expected: true
+	iisogram := IsIsogram("iisogram")  // expected: false
+	noob := IsIsogram("noob")  // expected: false
+	
+	// PRINT RESULTS----------------------------------------------------
+	fmt.Printf("word: isogram, expected: true, got: %v \n", isogram)
+	fmt.Printf("word: iisogram, expected: false, got: %v \n", iisogram)
+	fmt.Printf("word: '', expected: true, got: %v \n", emty)
+	fmt.Printf("word: noob, expected: false, got: %v \n", noob)
+	// -----------------------------------------------------------------	
+}
+
