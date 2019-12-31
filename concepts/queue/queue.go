326a0e43a58f4f40baa11ebee59bce21d49ea36e
Lukas Jocham <jochamlu@gmail.com>
2019-12-31 15:40:53 +0100

    update queue

--- concepts/queue/queue.go
+++ concepts/queue/queue.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -24,7 +25,7 @@ func (q *queue) enqueue(e *entity) {
 // remove from the front
 func (q *queue) dequeue() []*entity {
 	if q.count != 0 {
-		q.collection = remove(q.collection, 1)
+		q.collection = remove(q.collection, 0)
 		q.count--
 		return q.collection
 	}
@@ -32,14 +33,14 @@ func (q *queue) dequeue() []*entity {
 }
 
 // get next element to dequeue
-func (q *queue) front() []*entity {
+func (q *queue) peek() []*entity {
 	if q.count != 0 {
 		return q.collection[:1]
 	}
 	return nil
 }
 
-func (q *queue) len() int64 {
+func (q *queue) qcount() int64 {
 	if q.count != 0 {
 		return q.count
 	}
@@ -55,18 +56,27 @@ func remove(collection []*entity, first int) []*entity {
 }
 
 func main() {
+	start := time.Now()
+	defer execTime(start, "queue")
 	myq := Newqueue()
 
-	// Enqueue 10 elements
-	for i := 0; i < 10; i++ {
+	// adding 100 elements to the queue, queuecount should be 100
+	for i := int64(0); i < int64(1000); i++ {
 		myq.enqueue(&entity{value: int64(i), timestamp: time.Now()})
 	}
+	fmt.Printf("First Entity: %v \n", myq.peek())
+	fmt.Printf("Queue Count: %v \n", myq.qcount())
 
-	fmt.Printf("First Element %v \n", myq.front())
-	fmt.Printf("Current Lenght: %v \n", myq.len())
-	fmt.Printf("Current Queue: %v \n", myq)
-	myq.dequeue()
-	fmt.Printf("First Element %v \n", myq.front())
-	fmt.Printf("Current Lenght: %v \n", myq.len())
-	fmt.Printf("Current Queue: %v \n", myq)
+	// Removing 60 Entitys from the queue, current queue should be 40 elements
+	for i := int64(0); i < int64(60); i++ {
+		myq.dequeue()
+	}
+	fmt.Printf("First Entity: %v \n", myq.peek())
+	fmt.Printf("Queue Count: %v \n", myq.qcount())
+}
+
+// execTime tracks the execution time of a given function
+func execTime(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("INFO: exec time: %v took %v", name, elapsed)
 }
