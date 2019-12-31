20e9be09e38d3f359cc330e06316814b5fd4e74d
Lukas Jocham <jochamlu@gmail.com>
2019-12-31 14:37:09 +0100

    add queue. not really final but hey

--- concepts/queue/queue.go
+++ concepts/queue/queue.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type entity struct {
+	value     int64
+	timestamp time.Time
+}
+
+type queue struct {
+	collection []*entity
+	count      int64
+}
+
+// add to rear of slice
+func (q *queue) enqueue(e *entity) {
+	q.collection = append(q.collection[:q.count], e)
+	q.count++
+}
+
+// remove from the front
+func (q *queue) dequeue() []*entity {
+	if q.count != 0 {
+		q.collection = remove(q.collection, 1)
+		q.count--
+		return q.collection
+	}
+	return nil
+}
+
+// get next element to dequeue
+func (q *queue) front() []*entity {
+	if q.count != 0 {
+		return q.collection[:1]
+	}
+	return nil
+}
+
+func (q *queue) len() int64 {
+	if q.count != 0 {
+		return q.count
+	}
+	return 0
+}
+
+func Newqueue() *queue {
+	return &queue{}
+}
+
+func remove(collection []*entity, first int) []*entity {
+	return append(collection[:first], collection[first+1:]...)
+}
 
 func main() {
-	fmt.Println("Multi Worker Queue in go")
+	myq := Newqueue()
+
+	// Enqueue 10 elements
+	for i := 0; i < 10; i++ {
+		myq.enqueue(&entity{value: int64(i), timestamp: time.Now()})
+	}
+
+	fmt.Printf("First Element %v \n", myq.front())
+	fmt.Printf("Current Lenght: %v \n", myq.len())
+	fmt.Printf("Current Queue: %v \n", myq)
+	myq.dequeue()
+	fmt.Printf("First Element %v \n", myq.front())
+	fmt.Printf("Current Lenght: %v \n", myq.len())
+	fmt.Printf("Current Queue: %v \n", myq)
 }
