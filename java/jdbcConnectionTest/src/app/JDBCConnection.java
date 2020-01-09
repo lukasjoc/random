e0731b718f590fe03eff11ee9054a9e1365f731c
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-01-09 15:21:55 +0100

    make it static usable, in a singleton way

--- java/jdbcConnectionTest/src/app/JDBCConnection.java
+++ java/jdbcConnectionTest/src/app/JDBCConnection.java
@@ -9,24 +9,19 @@ import java.io.IOException;
 public class JDBCConnection {
    public static Connection conn = null;
    public static DatabaseMetaData dbMeta;
-   // getConnection that establishes a new jdbc mysql connection based on the connection values, returns a new connection runs as a kind of factory
-   public static Connection newConn(final String url, final String username, final String passwd) throws SQLException, ClassNotFoundException, IOException {
-      
-      if(conn == null) {
+
+   // getConnection that establishes a new jdbc mysql connection based on the
+   // connection values, returns a new connection runs as a kind of factory
+   public static Connection newConn(final String url, final String username, final String passwd)
+         throws SQLException, ClassNotFoundException, IOException {
+
+      if (conn == null) {
          try {
             conn = DriverManager.getConnection(url, username, passwd);
             if (conn == null) {
                System.err.println("could not get new connection to database");
             }
-   
             System.out.println("connection up and running...");
-            dbMeta = conn.getMetaData();
-            System.out.println(dbMeta);
-   
-            if (conn != null && !conn.isClosed()) {
-               conn.close();
-               System.out.println("closed connection to database..");
-            }
          } catch (SQLException e) {
             e.printStackTrace();
          }
--- java/jdbcConnectionTest/src/app/Main.java
+++ java/jdbcConnectionTest/src/app/Main.java
@@ -3,33 +3,38 @@ package app;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
-
 
 /**
  * Main
  */
-public class Main extends JDBCConnection {
+public class Main {
 
-  public static void main(final String[] args) throws ClassNotFoundException, IOException {
+  public static void main(final String[] args) 
+      throws ClassNotFoundException, IOException {
+
     final FileInputStream fs;
     final Properties p;
     final String url, username, passwd, connPropsFile;
     try {
-       connPropsFile = "config/c.properties";
-       fs = new FileInputStream(new File(connPropsFile));
-       p = new Properties();
-       p.load(fs);
+      connPropsFile = "config/c.properties";
+      fs = new FileInputStream(new File(connPropsFile));
+      p = new Properties();
+      p.load(fs);
 
-       url = (String) p.get("URL");
-       username = (String) p.get("USER");
-       passwd = (String) p.get("PASSWD");
+      url = (String) p.get("URL");
+      username = (String) p.get("USER");
+      passwd = (String) p.get("PASSWD");
 
-       newConn(url, username, passwd);
-       fs.close();
+      Connection conn = JDBCConnection.newConn(url, username, passwd);
+      System.out.println(conn.getMetaData());
+      
+
+      fs.close();
     } catch (final SQLException e) {
-       e.printStackTrace();
+      e.printStackTrace();
     }
   }
 }
