ffa95520afff101254ba7f2087b793b349a20d69
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-01-09 15:26:25 +0100

    enhance readability

--- java/jdbcConnectionTest/src/app/JDBCConnection.java
+++ java/jdbcConnectionTest/src/app/JDBCConnection.java
@@ -14,20 +14,23 @@ public class JDBCConnection {
    // connection values, returns a new connection runs as a kind of factory
    public static Connection newConn(final String url, final String username, final String passwd)
          throws SQLException, ClassNotFoundException, IOException {
-
-      if (conn == null) {
-         try {
-            conn = DriverManager.getConnection(url, username, passwd);
-            if (conn == null) {
-               System.err.println("could not get new connection to database");
-            }
-            System.out.println("connection up and running...");
-         } catch (SQLException e) {
-            e.printStackTrace();
-         }
+            
+      // reuse old connection
+      if (conn != null) {
+         System.out.println("reuse connecton: " + dbMeta.getURL());
          return conn;
       }
-      System.out.println("reuse connecton: " + dbMeta.getURL());
+
+      // new connection
+      try {
+         conn = DriverManager.getConnection(url, username, passwd);
+         if (conn == null) {
+            System.err.println("could not get new connection to database");
+         }
+         System.out.println("connection up and running...");
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
       return conn;
    }
 }
