8fc543f7c267bf35d3710ab0eb69a1ec0d3c4c6c
Lukas Jocham <jochamlu@gmail.com>
2020-01-10 21:40:21 +0100

    add basic histogram

---
+++ concepts/gen/histogram.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func wordCount(str string) map[string]int {
+
+	words := strings.Fields(str)
+	counts := make(map[string]int)
+
+	// check if word currently in the map,
+	// if so increment by 1 else
+	// add new element with value 1
+	for _, word := range words {
+		if _, inMap := counts[word]; !inMap {
+			counts[word] = 1
+		} else {
+			counts[word] += 1
+		}
+	}
+	return counts
+}
+
+func main() {
+	test := "Hello, World!"
+	fmt.Printf("Histogram: %v \n", test)
+	for index, element := range wordCount(test) {
+		fmt.Printf("%v => %v\n", index, element)
+	}
+}
