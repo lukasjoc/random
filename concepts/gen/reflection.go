f8cd8db1f3b782d1a2d61f7bea823802ad2f0251
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-01-19 08:35:07 +0100

    fix getany and remove functions

--- concepts/gen/reflection.go
+++ concepts/gen/reflection.go
@@ -5,12 +5,21 @@ import (
 	"reflect"
 )
 
+type Slicing interface {
+	GetLast()
+	GetFirst()
+	GetAny()
+	Remove()
+	RemoveLast()
+}
+
 // anyToSlice converts any input type given the interface{} builtin type to a full slice variant,
 // creates the slice with the same lenght and values. !!The slice that is given for reflection IS NOT the slice
 // receiving back after the reflection process but a TIGHT copy...
 func anyToSlice(t interface{}) (out []interface{}, ok bool) {
 	slice, success := typeCheck(t, reflect.Slice)
 	success = ReflectionFailure(success)
+
 	sliceLen := slice.Len()
 
 	out = make([]interface{}, sliceLen)
@@ -57,30 +66,26 @@ func GetFirst(t interface{}) interface{} {
 
 // GetAny returns a slice of values from the input slice of any type specified in the elems slice. List is sorted if the input slice is sorted
 // Does not sort the result...
-// FIXME: this throws arbitrary errors.. lets fix this
-func GetAny(t interface{}, elems []int) (rt interface{}) {
+func GetAny(t interface{}, elems []int) (temp []interface{}) {
 	slice, _ := anyToSlice(t)
-	temp, _ := anyToSlice(rt)
 
-	for _, index := range elems {
+	for _, i := range elems {
 		for j, elem := range slice {
-			if index == j {
+			if i == j {
 				temp = append(temp, elem)
 			}
 		}
 	}
+
 	return temp
 }
 
-// Remove removes a list of elements from a given input slice of any type any. Order cannot guaranteed after reflection and
-// deletion process...
-// FIXME: this function does not behave the way its indented to....
-func Remove(t interface{}, elems []int) interface{} {
+// Remove removes one element from the front of a given input slice of any type any. Order cannot be guaranteed after reflection and
+// deletion process ...
+func Remove(t interface{}, elem int) interface{} {
 	slice, _ := anyToSlice(t)
+	slice = append(slice[:elem], slice[elem+1:]...)
 
-	for _, elem := range elems {
-		slice = append(slice[:elem], slice[elem-1:]...)
-	}
 	return slice
 }
 
@@ -91,25 +96,26 @@ func RemoveLast(t interface{}) interface{} {
 
 	last := len(slice) - 1
 	slice = append(slice[:last], slice[last+1:]...)
+
 	return slice
 }
 
 func main() {
 	var testData []interface{}
-	testData = append(testData, []string{"test", "test"})
-	testData = append(testData, []rune{'r', 'l'})
-	testData = append(testData, []int{1, 3})
-	testData = append(testData, []int64{232, 2323})
-	testData = append(testData, []float32{13.00, 245.99})
-	testData = append(testData, []float64{13.17, 233.33})
-	testData = append(testData, []bool{false, true})
+	testData = append(testData, []string{"test", "test", "test4"})
+	testData = append(testData, []rune{'r', 'l', 't'})
+	testData = append(testData, []int{1, 3, 6})
+	testData = append(testData, []int64{232, 2323, 923})
+	testData = append(testData, []float32{13.00, 245.99, 77.66})
+	testData = append(testData, []float64{13.17, 233.33, 45.67})
+	testData = append(testData, []bool{false, true, true})
 
 	for _, c := range testData {
 		first := GetFirst(c)
 		last := GetLast(c)
-		// any := GetAny(c, []int{1})
-		remove := Remove(c, []int{0})
-		removeLast := Remove(c, []int{0})
-		fmt.Printf("Slice: %v, GetFirst %v, GetLast: %v, GetAny: %v, Remove: %v, RemoveLast: %v \n", c, first, last, "", remove, removeLast)
+		any := GetAny(c, []int{0})
+		remove := Remove(c, 0)
+		removeLast := RemoveLast(c)
+		fmt.Printf("Slice: %v, GetFirst %v, GetLast: %v, GetAny: %v, Remove: %v, RemoveLast: %v \n", c, first, last, any, remove, removeLast)
 	}
 }
