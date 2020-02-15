2c4fc4eff123106404c9f3b26710f6359e7ddc52
Lukas Jocham <jochamlu@gmail.com>
2020-02-15 18:51:16 +0100

    largest prime factor of a large number

---
+++ proj_euler/largest_prime_fac.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def prime_facts(n) -> list:
+    prime_facts = []
+    d = 2
+    while (d*d <= n):
+        while (n > 1):
+            while (n%d) == 0:
+                prime_facts.append(d)
+                n /= d
+            d += 1
+
+    return prime_facts
+
+if __name__ == "__main__":
+    print(max(prime_facts(600851475143)))
