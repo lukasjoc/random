44ce3c4ef0be751d6dd93b29fa457ca690e8c1e7
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-02-25 13:11:55 +0100

    update sum of square solution with the cool gaussian formular one

--- proj_euler/sum_square_diff.py
+++ proj_euler/sum_square_diff.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-def sum_square_diff(limit: int) -> int:
-    a, b = 0, 0
-    for n in range(1, limit+1):
-        a += n
-        b += n**2
-    return int( (a**2) - (b) )
-
+def sum_square_diff(n: int) -> int:
+    sum_n = (n*(n+1)/2)
+    return int((sum_n*sum_n)-(n*(n+1)*(2*n+1)/6))
 
 if __name__ == "__main__":
-  print(sum_square_diff(limit=100))
+  print(sum_square_diff(n=100))
