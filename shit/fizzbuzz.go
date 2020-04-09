e3ff3c6d6571e260ebe5dafbf5b667fa1e1903cc
lukasjoc <jochamlu@gmail.com>
2020-04-09 09:45:53 +0200

    addddd

--- shit/fizzbuzz.go
+++ shit/fizzbuzz.go
@@ -3,16 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	nums := make([]int, 1000000)
-	for cool := range nums {
-		if (cool % 15) == 0 {
-			fmt.Println("FizzBuzz")
-		} else if (cool % 5) == 0 {
-			fmt.Println("Fizz")
-		} else if (cool % 3) == 0 {
-			fmt.Println("Buzz")
-		} else {
-			fmt.Println(cool)
+	for cool := 1; cool < 100; cool++ {
+		dofiz(cool)
+	}
+
+	i := 100
+	for {
+		if i >= 100 {
+			break
 		}
+		i++
+		dofiz(i)
+	}
+	cooaaaaaa := make([]int, 101)
+	for cool := range cooaaaaaa {
+		dofiz(cool)
+	}
+}
+
+func dofiz(cool int) {
+	if (cool % 15) == 0 {
+		fmt.Println("FizzBuzz")
+	} else if (cool % 5) == 0 {
+		fmt.Println("Fizz")
+	} else if (cool % 3) == 0 {
+		fmt.Println("Buzz")
+	} else {
+		fmt.Println(cool)
 	}
 }
