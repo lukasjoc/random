b60cbe3123362cc2a227eada336688d37905f1d4
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2020-07-13 09:06:49 +0200

    delete python stuff add some other stuff merge some other stuff do feature a

--- perl5/threading.pl
+++ perl5/threading.pl
@@ -7,8 +7,8 @@ use threads;
 my $thr = threads->new(\&sub1, "Param 1", "Param 2"); 
 
 sub sub1 {
-  sleep 4;
-  print "In the thread:".join(",", @_),"\n"; 
+	sleep 4;
+	print "In the thread:".join(",", @_),"\n"; 
 }
 
 for (my $c = 0; $c < 10; $c++) {
