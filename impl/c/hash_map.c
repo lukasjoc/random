4d54e63a6e63f1e0ec693d70d70edfc9f93b06ac
lukasjoc <jochamlu@gmail.com>
2021-02-18 23:11:33 +0100

    update a bit but it is still segvaultig

--- impl/c/hash_map.c
+++ impl/c/hash_map.c
@@ -1,13 +1,14 @@
-#include  <stdio.h>
+// basic hashmap instance with 100 free buckets
+// for now just static and not automagically resizable
 
+#include <stdio.h>
+#include <stdlib.h>
 // pair of key and value ints only
 // and keys are / should be mostly unique and uniform
 // [10] -> [1, 2, 3, 4|4-> 5, NULL, 6, 7, NULL, 9, NULL]
 typedef struct pair {
-  // int *key;
-  // int *value;
-  int key;
-  int value;
+  int *key;
+  int *value;
 } pair;
 
 // hashmap datastructure
@@ -26,7 +27,7 @@ typedef struct hashmap {
   int numberof_buckets;
 
   // key value pairs of connected ints
-  pair buckets[];
+  pair *buckets[];
 } hashmap;
 
 // TODO: add, remove, get -> dict_opts
@@ -36,63 +37,73 @@ typedef struct hashmap {
 // m -> number of buckets
 // a -> load factor of hash table(n/m)
 
+
+// TODO: tomorrow rebuild to this usage behavour
+// insert({"firstNum", 1});
+// insert({"firstNum", 1});
+// insert({"firstNum", 1}); 
+
 int probed_index(hashmap *hm, int *key ) {
   int mod_index = *key % hm->numberof_buckets;
-  pair mod_pair = hm->buckets[mod_index];
+
+  pair *mod_pair = hm->buckets[mod_index];
+  if (mod_pair == NULL ) return 1;
+  if (mod_pair != NULL) return mod_index;
 
   // if it is direcly accessable then just use it
-  if (mod_pair.key != 0) return mod_index;
-
+  //   if (mod_pair->key != NULL) return mod_index;
   // if we have to search for a diffrent index to use
   // we will single step through the thing
   // TODO: maybe we also take the load factor into consideration
   // and reszie and append accordingly but that is for later
-  while( (mod_pair.key != 0) && (mod_pair.key != *key) ){
+  while (
+    (mod_pair->key != NULL)
+    && (mod_pair->key != key) )
     mod_index = (mod_index +1) % hm->numberof_buckets;
-  }
   return mod_index;
 }
 
 // insert into hashmap
-void insert(hashmap *hm, int *value) {
-  int index = probed_index(hm, value);
-  hm->buckets[index].key = index;
-  hm->buckets[index].value = *value;
+void insert(hashmap *hm, int key) {
+  int index = probed_index(hm, &key);
+
+  // allocate memory for new key value pair in buckets
+  pair *p = malloc(sizeof(pair));
+
+  // update values in the pair
+  // see what happens if i take the addresses with &
+  p->key = &index;
+  p->value = &key;
+
+  // update struct bucket
+  hm->buckets[index] = p;
 }
 
-// get value by key
+
 //int get(hashmap *hm, *key) {
-//  return hm->buckets.value
+//  
 //}
 
-// TODO: initialize with size
+// TODO: free the memory of the key value pair
+// DELETE FROM hashmap
+// void delete(hashmap *hm, int key) { }
 
-int main() {
-  // basic hashmap instance with 100 free buckets
-  // for now just static and not automagically resizable
-
-  hashmap yolomap = {
-    .numberof_buckets = 100,
-  };
-
-  printf("yolomap: %p\n", (void *)&yolomap);
-
-  // initialize with zeros
-  // for (int i = 0; i <= yolomap.numberof_buckets; i++) {
-  //  yolomap.buckets[i].key = 0;// NULL;
-  //  yolomap.buckets[i].value = 0; //NULL;
-  //}
-
-  for(int i = 0; i < yolomap.numberof_buckets; i++) {
-    insert(&yolomap, &i);
-  }
-  for(int i = 0; i < yolomap.numberof_buckets; i++) {
-    if (yolomap.buckets[i].key != 0) {
-      //printf("{ %p: %p\n }", (void*)&yolomap.buckets[i].key, (void*)&yolomap.buckets[i].value);
-      printf("{ %d: %d\n }", yolomap.buckets[i].key, yolomap.buckets[i].value);
-    }
-  }
-  return 0;
+int get(hashmap *hm, int key) {
+  if (hm->buckets[key] == NULL) return 1;
+  return *hm->buckets[key]->value;
 }
 
+int main() {
+  hashmap yolomap = {
+    .numberof_buckets = 10,
+  };
+
+  insert(&yolomap, 10);
+
+  //for(int i = 0; i < yolomap.numberof_buckets; i++) {
+  // printf("Key Inserted: %d", get(&yolomap, 10));
+  //}
+
+  return 0;
+}
 
