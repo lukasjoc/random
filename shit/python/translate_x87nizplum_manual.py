2ab051af0bfd37246345694023de1b1098ac4056
lukasjoc <jochamlu@gmail.com>
2021-03-06 12:43:14 +0100

    make the translation better with model

--- shit/python/translate_x87nizplum_manual.py
+++ shit/python/translate_x87nizplum_manual.py
@@ -4,27 +4,22 @@
 import sys
 from PIL import Image
 import pytesseract
-# from googletrans import Translator
 import translators as ts
 
 if len(sys.argv) > 1:
     IMAGE_PATH = str(sys.argv[1])
 else:
     print("please provide an image of the original manual you got in the package")
+    print("""you may also need the trained model for chinese
+        https://github.com/tesseract-ocr/tessdata/blob/master/chi_sim.traineddata""")
     sys.exit(1)
 
-print(f"Now extracting from {IMAGE_PATH}...", sep="\n")
 image = Image.open(IMAGE_PATH)
-image_text = pytesseract.image_to_string(image)
+image.load()
 
-
+image_text = pytesseract.image_to_string(image, lang="chi_sim")
 print("Now translating...", sep="\n")
-# trans = Translator()
-# res = trans.translate(image_text, dest="de")
-res = ts.google(image_text)
 
-print("res", res)
-# translated = str(res.text)
-
-# print(translated, sep="\n")
+res = ts.google(image_text, source_lang="chinese")
+print(res)
 
