468b29a44376b0c87b33053fd8f47605b8ba6b1a
lukasjoc <jochamlu@gmail.com>
2021-06-22 18:21:02 +0200

    exclude target

--- impl/rust/binary_search/target/doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"binary_search":{"doc":"","t":[5],"n":["main"],"q":["binary_search"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
-}');
-initSearch(searchIndex);
--- impl/rust/binary_search/target/doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["binary_search"] = {"name":"","files":["main.rs"]};
-createSourceSidebar();
